92be40c9d1ee1bffabaf19b0d8d962a9021dbf85
Shri <Shristi.Saha@godigit.com>
2024-08-21 15:04:55 +0530

    [Shristi] ADD : Created Department Model.

---
+++ src/main/java/com/example/example1/model/DepartmentModel.java
@@ -0,0 +1,19 @@
+package com.example.example1.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class DepartmentModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private Long id;
+
+    private String deptName;
+    private String deptCode;
+}
